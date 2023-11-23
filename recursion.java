31c42c13627dce93b2916097883bbc8adb2ea6a7
sopheanitaRit <2022205rit@aupp.edu.kh>
2023-11-23 09:49:47 +0700

    recursion.java
    H. Implement** Merge Sort Function ** using recursion approach.

--- recursion.java
+++ recursion.java
@@ -1,15 +1,24 @@
+import java.util.Scanner;
+
 public class recursion {
-  public static void main(String[] args) {
 
-    //call your recursion fuction
-    //print result
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
 
-   }
-    
-    public double.... recursionFunction {
-      //implement your case algorithm
-      return....
+        double result = factorial(number);
+        System.out.println("Factorial of " + number + " is: " + result);
+
+        scanner.close();
     }
-  
+
+    public static double factorial(int n) {
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
 }
-    
